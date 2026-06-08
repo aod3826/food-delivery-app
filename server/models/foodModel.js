47c2889a3ed0a69b15cded1569835faca7b77290
aod3826 <pantipa3826@gmail.com>
2026-06-08 23:29:12 +0700

    Migrate backend data models to Firestore

--- server/models/foodModel.js
+++ server/models/foodModel.js
@@ -1,29 +1,40 @@
-import mongoose from "mongoose";
+import { addDocument, deleteDocument, getCollection, getDocument } from "../config/db.js";
 
-const foodSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true
-  },
-})
+const collectionName = "foods";
 
+class foodModel {
+  constructor(foodData) {
+    this.name = foodData.name;
+    this.description = foodData.description;
+    this.price = Number(foodData.price);
+    this.image = foodData.image;
+    this.category = foodData.category;
+  }
 
-const foodModel = mongoose.models.food || mongoose.model("food", foodSchema)
+  async save() {
+    const food = await addDocument(collectionName, {
+      name: this.name,
+      description: this.description,
+      price: this.price,
+      image: this.image,
+      category: this.category
+    });
+
+    this._id = food._id;
+    return this;
+  }
+
+  static async find() {
+    return getCollection(collectionName);
+  }
+
+  static async findById(id) {
+    return getDocument(collectionName, id);
+  }
+
+  static async findByIdAndDelete(id) {
+    return deleteDocument(collectionName, id);
+  }
+}
 
 export default foodModel;
--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,15 +1,48 @@
-import mongoose from "mongoose";
+import { addDocument, deleteDocument, getCollection, queryCollection, updateDocument } from "../config/db.js";
 
-const orderSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  items: { type: Array, required: true },
-  amount: { type: Number, required: true },
-  address: { type: Object, required: true },
-  status: { type: String, default: "Food Processing" },
-  date: { type: Date, default: Date.now() },
-  payment: { type: Boolean, default: false }
-})
+const collectionName = "orders";
 
-const orderModel = mongoose.model.order || mongoose.model("order", orderSchema);
+class orderModel {
+  constructor(orderData) {
+    this.userId = orderData.userId;
+    this.items = orderData.items;
+    this.amount = Number(orderData.amount);
+    this.address = orderData.address;
+    this.status = orderData.status || "Food Processing";
+    this.date = orderData.date || Date.now();
+    this.payment = orderData.payment || false;
+  }
+
+  async save() {
+    const order = await addDocument(collectionName, {
+      userId: this.userId,
+      items: this.items,
+      amount: this.amount,
+      address: this.address,
+      status: this.status,
+      date: this.date,
+      payment: this.payment
+    });
+
+    this._id = order._id;
+    return this;
+  }
+
+  static async find(filter = {}) {
+    if (filter.userId) {
+      return queryCollection(collectionName, "userId", "EQUAL", filter.userId);
+    }
+
+    return getCollection(collectionName);
+  }
+
+  static async findByIdAndUpdate(id, data) {
+    return updateDocument(collectionName, id, data);
+  }
+
+  static async findByIdAndDelete(id) {
+    return deleteDocument(collectionName, id);
+  }
+}
 
 export default orderModel;
